069c749efc6532474e8ce2b5df886839f4f0ca29
Madi <madi_gaziz@mail.ru>
2023-08-16 21:56:30 +0600

    Сократил код в классе Main, а именно распределил код по функциям, так же сделал проверку если пользователь введет неверный запрос(индекс)

--- main.py
+++ main.py
@@ -45,15 +45,10 @@ class Animal:
 
         if age <= 5:
             self.age_strategy = YoungAgeStrategy()
-            print('work')
         elif age <= 10:
             self.age_strategy = MiddleAgeStrategy()
-            print('work two')
-
-
         else:
             self.age_strategy = OldAgeStrategy()
-            print('work three')
 
 
     def __str__(self):
@@ -83,6 +78,7 @@ class Animal:
     def play(self):
         print(f'Вы играете с {self.name}')
         if self.random_numbers():
+            print(f'Внезапное событие. ВАШ {self.name} ТРАВМИРОВАЛСЯ!')
             self.hunger = self.age_strategy.apply_decrease(self.hunger)
             self.mood = self.age_strategy.apply_decrease(self.mood)
             self.health = self.age_strategy.apply_decrease(self.health)
@@ -99,7 +95,7 @@ class Animal:
 
 
     def info(self):
-        return print(f'\nХарактеристики: {self.hunger}, {self.mood}, {self.health}')
+        return print(f'{"="*40}\nХарактеристики:\nГолод:{self.hunger}\nНастроение:{self.mood}\nЗдоровье: {self.health}\n{"="*40}')
 
     @staticmethod
     def check_statistics(animal):
@@ -113,16 +109,21 @@ class Main:
 
     def run(self):
         while True:
-            print('Добро пожаловать в приложение.')
             if not self.animals:
-                print('Прежде чем начать игать, пожалуйста создайте животное\n')
+                print('Прежде чем начать играть, пожалуйста, создайте животное\n')
                 self.add_animals()
             else:
-                for animal in self.animals:
-                    print(f'[1] Имя: {animal.name}, Возвраст {animal.age}')
-                choose_animal = int(input('\nВыберите животное из списка: ')) - 1
+                for idx, animal in enumerate(self.animals, start=1):
+                    print(f'{"=" * 40}\n[{idx}] Имя: {animal.name}, Возраст {animal.age}\n{"=" * 40}')
 
-                choose = input('Какое действие вы хотите сделать?\n[1]Покормить животное\n[2]Поиграть с животным\n[3]Лечить животное')
+                choose_animal = self.get_valid_animal_choice()
+
+                valid_actions = ['1', '2', '3', '4']
+                choose = self.get_valid_choice(
+                    f'Какое действие вы хотите сделать?\n{"=" * 40}\n[1] Покормить животное\n[2] Поиграть с животным\n[3] Лечить животное\n[4] Добавить животное\n{"=" * 40}\n',
+                    valid_actions
+                )
+
                 id_animal = self.animals[choose_animal]
                 match choose:
                     case '1':
@@ -134,6 +135,25 @@ class Main:
                     case '4':
                         self.add_animals()
                 Animal.check_statistics(id_animal)
+
+    def get_valid_choice(self, message, valid_choices):
+        while True:
+            user_input = input(message)
+            if user_input in valid_choices:
+                return user_input
+            else:
+                print('Неверный ввод. Пожалуйста, выберите корректное действие.')
+
+    def get_valid_animal_choice(self):
+        while True:
+            try:
+                choose_animal = int(input('\nВыберите животное из списка (введите номер): ')) - 1
+                if 0 <= choose_animal < len(self.animals):
+                    return choose_animal
+                else:
+                    print('Неверный номер. Пожалуйста, введите корректный номер.')
+            except ValueError:
+                print('Неверный ввод. Пожалуйста, введите номер животного.')
 
     def add_animals(self):
         name = input('Введите имя: ')
@@ -150,4 +170,5 @@ class Main:
 
 
 if __name__ == '__main__':
+    print('Добро пожаловать в приложение.')
     Main().run()
